9d94618c6ec80ea2de72274bceae4b717975c42d
shayuna <shaiz2206@gmail.com>
2017-09-27 00:31:50 +0300

    another time

--- routes/mainapi.js
+++ routes/mainapi.js
@@ -1,11 +1,15 @@
 var express=require("express");
+var path=require("path");
 var router=express.Router();
 
 router.get("/second/*",function(req,res,next){
     res.render ("../public/main.jade");
 });
+router.get("/third/*",function(req,res,next){
+    res.sendFile (path.resolve("./public/t1.htm"));
+});
 router.get("/*",function(req,res,next){
-    res.end ("all things must pass")
+    res.end ("the joke is on you");
 });
 
 module.exports=router;
